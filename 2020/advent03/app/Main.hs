e126b8060eb95629840fac5250cedee4edac6d8b
Joel Hermanns <joel.hermanns@gmail.com>
2020-12-05 22:20:35 +0100

    feat(2020,03): add solution for day 3

--- 2020/advent03/app/Main.hs
+++ 2020/advent03/app/Main.hs
@@ -3,4 +3,32 @@ module Main where
 import AdventOfCode
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "reading input.txt..."
+  input <- readFile "input.txt"
+  let m = parseMap $ lines input
+      resultOne = partOne m
+      resultTwo = partTwo m
+  putStrLn $ "solution for part one is " ++ show resultOne
+  putStrLn $ "solution for part two is " ++ show resultTwo
+
+partOne :: Map -> Int
+partOne m = traverseM m (down 1 . right 3)
+
+partTwo :: Map -> Int
+partTwo m = foldl (*) 1 $ map (traverseM m) [step1, step2, step3, step4, step5]
+  where
+    step1 = down 1 . right 1
+    step2 = down 1 . right 3
+    step3 = down 1 . right 5
+    step4 = down 1 . right 7
+    step5 = down 2 . right 1
+
+traverseM :: Map -> (Position -> Position) -> Int
+traverseM m step = length $ filter isTree $ goToBottom m (Position 0 0) step
+
+goToBottom :: Map -> Position -> (Position -> Position) -> [Item]
+goToBottom m pos step = h m pos step [] False
+  where
+    h m pos step res True = res
+    h m pos step res False = h m (step pos) step (res ++ [positionItem m pos]) (atBottom m (step pos))
--- 2020/advent03/src/AdventOfCode.hs
+++ 2020/advent03/src/AdventOfCode.hs
@@ -1,6 +1,61 @@
 module AdventOfCode
-    ( someFunc
+    ( Item(..)
+    , parseItem
+    , isTree
+    , Line(..)
+    , parseLine
+    , Map
+    , parseMap
+    , Position(..)
+    , positionItem
+    , right
+    , down
+    , atBottom
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+data Item = Tree | Square
+  deriving (Show)
+
+parseItem :: Char -> Item
+parseItem '#' = Tree
+parseItem '.' = Square
+parseItem _ = undefined
+
+isTree :: Item -> Bool
+isTree Tree = True
+isTree _    = False
+
+data Line = Line [Item]
+  deriving (Show)
+
+parseLine :: String -> Line
+parseLine = Line . map parseItem
+
+positionInLine :: Int -> Line -> Item
+positionInLine y (Line is) = is !! (y `mod` (length is))
+
+data Map  = Map [Line]
+ deriving (Show)
+
+parseMap :: [String] -> Map
+parseMap = Map . map parseLine
+
+data Position = Position Int Int
+  deriving (Show)
+
+positionItem :: Map -> Position -> Item
+positionItem (Map ls) (Position x y) = item
+  where
+    l = length ls
+    line = ls !! ( y `mod` l)
+    item = positionInLine x line
+
+atBottom :: Map -> Position -> Bool
+atBottom (Map ls) (Position _ y) = y >= length ls
+
+right :: Int -> Position -> Position
+right i (Position x y) = Position (x+i) y
+
+down :: Int -> Position -> Position
+down i (Position x y) = Position x (y+i)
