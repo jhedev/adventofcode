0a3be5ffc7e7c68a08b48a0a5e6b7847bf00a23c
Joel Hermanns <joel.hermanns@gmail.com>
2020-12-06 10:58:58 +0100

    feat(2020,06): add solution for day 6

--- 2020/advent06/app/Main.hs
+++ 2020/advent06/app/Main.hs
@@ -3,4 +3,10 @@ module Main where
 import AdventOfCode
 
 main :: IO ()
-main = someFunc
+main = do
+  input <- readFile "input.txt"
+  let groups = parseFile input
+      resultOne = sum . map (length . anyoneYes) $ groups
+      resultTwo = sum . map (length . everyoneYes) $ groups
+  putStrLn $ "solution for part one is " ++ show resultOne
+  putStrLn $ "solution for part two is " ++ show resultTwo
--- 2020/advent06/src/AdventOfCode.hs
+++ 2020/advent06/src/AdventOfCode.hs
@@ -1,6 +1,42 @@
 module AdventOfCode
-    ( someFunc
+    ( Group
+    , questions
+    , numPersons
+    , anyoneYes
+    , everyoneYes
+    , parseLine
+    , parseGroup
+    , parseFile
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.HashMap.Strict as Map
+import Data.List.Split
+
+data Group = Group Int (Map.HashMap Char Int) deriving Show
+
+questions :: Group -> [Char]
+questions (Group _ m) = Map.keys m
+
+numPersons :: Group -> Int
+numPersons (Group p _) = p
+
+anyoneYes :: Group -> [Char]
+anyoneYes (Group _ m) = Map.keys m
+
+everyoneYes :: Group -> [Char]
+everyoneYes (Group p m) = Map.foldlWithKey' f [] m
+  where
+    f acc q i | p == i = q:acc
+              | otherwise = acc
+
+parseLine :: Map.HashMap Char Int -> String -> Map.HashMap Char Int
+parseLine = foldl f
+  where
+    f :: Map.HashMap Char Int -> Char -> Map.HashMap Char Int
+    f m c = Map.insertWith (\_ o -> o+1) c 1 m
+
+parseGroup :: [String] -> Group
+parseGroup persons = Group (length persons) $ foldl parseLine Map.empty persons
+
+parseFile :: String -> [Group]
+parseFile = map parseGroup . splitWhen (== "") . lines
