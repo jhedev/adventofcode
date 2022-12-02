8cea5dd0bada015fde9df6722b1e7d346f6d2d12
Joel Hermanns <joel.hermanns@gmail.com>
2022-12-02 19:26:22 +0100

    wip

--- 2021/a01/src/main.rs
+++ 2021/a01/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let reader = read_lines("input.txt");
+    let lines = reader.collect()::<Result<_,_>>().unwrap();
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
